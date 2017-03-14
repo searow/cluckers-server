1dde13cfcd52c221e5a466a1c4ca9108e68cac12
Chris Ro <christopher.j.ro@gmail.com>
2017-03-14 02:08:36 -0700

    Rework to use new classes

--- cluckers.php
+++ cluckers.php
@@ -6,10 +6,16 @@
 
 namespace Cluckers;
 
-use Cluckers\Database\DatabaseAccessor;
+require_once(__DIR__ . '/Cluckers/Config/Config.php');
+require_once(__DIR__ . '/Cluckers/Database/DatabaseAccessor.php');
 
-require_once __DIR__ . "/Cluckers/Database/DatabaseAccessor.php";
+use Cluckers\Database\DatabaseAccessor as DatabaseAccessor;
+use Cluckers\Config\Config as Config;
 
-$db = new DatabaseAccessor();
+// Load the config file and get the database path.
+$config = new Config(__DIR__ . '/config/config.json');
 
-print_r($db->getCluckState(1));
+// Create database accessor, request handler, and handle the actual request.
+$accessor = new DatabaseAccessor($config->getDatabasePath());
+$requestHandler = new RequestHandler($accessor);
+$requestHandler->processRequest();
