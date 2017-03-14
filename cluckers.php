7aae85d50d0f1cd7253c90c5f25b15ea84ed78c0
Chris Ro <christopher.j.ro@gmail.com>
2017-03-14 02:39:40 -0700

    Add require for RequestHandler

--- cluckers.php
+++ cluckers.php
@@ -8,9 +8,11 @@ namespace Cluckers;
 
 require_once(__DIR__ . '/Cluckers/Config/Config.php');
 require_once(__DIR__ . '/Cluckers/Database/DatabaseAccessor.php');
+require_once(__DIR__ . '/Cluckers/Request/RequestHandler.php');
 
 use Cluckers\Database\DatabaseAccessor as DatabaseAccessor;
 use Cluckers\Config\Config as Config;
+use Cluckers\Request\RequestHandler as RequestHandler;
 
 // Load the config file and get the database path.
 $config = new Config(__DIR__ . '/config/config.json');
